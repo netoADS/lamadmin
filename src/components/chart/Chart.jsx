0cc84618ded5e4b59bfff197649b1d32cc2f2600
pcastroneto <pcastroneto101105@gmail.com>
2024-05-30 03:46:29 -0300

    30-05-2024

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,8 +1,37 @@
 import "./chart.scss"
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+const data = [
+    { name: "Janeiro", Total: 1200 },
+    { name: "Fevereiro", Total: 2100 },
+    { name: "Março", Total: 800 },
+    { name: "Abril", Total: 1600 },
+    { name: "Maio", Total: 900 },
+    { name: "Junho", Total: 1700 },
+
+];
+
 
 const Chart = () => {
-    return(
-        <div className="chart">Chart</div>
+    return (
+        <div className="chart">
+            <div className="title">Last 6 Months(Revenue)</div>
+            <ResponsiveContainer width="100%" aspect={2 / 1}>
+                <AreaChart width={730} height={250} data={data}
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                    <defs>
+                        <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="name" stroke="gray"/>
+                    <CartesianGrid strokeDasharray="3 3" className="chartGrid"/>
+                    <Tooltip />
+                    <Area type="monotone" dataKey="Total" stroke="#8884d8" fillOpacity={1} fill="url(#total)" />
+                </AreaChart>
+            </ResponsiveContainer>
+        </div>
     )
 }
 
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,7 +2,8 @@ import "./featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-
+import  KeyboardArrowDownIcon  from "@mui/icons-material/KeyboardArrowDown";
+import  KeyboardArrowUpOutlined  from "@mui/icons-material/KeyboardArrowUpOutlined";
 
 const Featured = () => {
     return (
@@ -15,11 +16,34 @@ const Featured = () => {
                 <div className="featuredChart">
                     <CircularProgressbar value={70} text={"70%"} strokeWidth={5}/>
                 </div>
-                < p className="title">Total slaes made today</p>
+                < p className="title">Total sales made today</p>
                 < p className="amount">R$420</p>
                 < p className="desc">
                 Processamento de transações anteriores. Os últimos pagamentos podem não estar incluídos.
                 </p>
+                <div className="summary">
+                    <div className="item">
+                        <div className="itemTitle">Target</div>
+                        <div className="itemResult negative">
+                            <KeyboardArrowDownIcon fontSize="small"/>
+                            <div className="resultAmount"> R$12.4k</div>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="itemTitle">Last Week</div>
+                        <div className="itemResult positive">
+                            <KeyboardArrowUpOutlined fontSize="small"/>
+                            <div className="resultAmount"> R$12.4k</div>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="itemTitle">Last Month</div>
+                        <div className="itemResult positive">
+                            <KeyboardArrowUpOutlined fontSize="small"/>
+                            <div className="resultAmount"> R$12.4k</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
