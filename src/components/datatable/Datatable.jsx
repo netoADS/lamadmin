03f9d32043b40fe37a3182e6b863d05f54010866
pcastroneto <pcastroneto101105@gmail.com>
2024-06-05 00:15:39 -0300

    04-05-24

---
+++ src/components/datatable/Datatable.jsx
@@ -0,0 +1,36 @@
+import { DataGrid } from "@mui/x-data-grid"
+import { userColumns, userRows } from "../../datatableource";
+import "./datatable.scss"
+
+
+const Datatable = () => {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <div className="viewButton">View</div>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      }
+    }
+  ]
+  return (
+    <div className="datatable">
+      <DataGrid
+        rows={userRows}
+        columns={userColumns.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+
+export default Datatable;
--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,12 +1,18 @@
 import "./list.scss"
-
+import Sidebar from "../../components/Sidebar/Sidebar"
+import Navbar from "../../components/Navbar/Navbar"
+import Datatable from "../../components/datatable/Datatable"
 
 const List = () => {
-    return (
-        <div className="list">
-            <h1 className="title">List</h1>
-        </div>
-    );
-};
+  return (
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <Datatable/>
+      </div>
+    </div>
+  )
+}
 
-export default List;
+export default List
